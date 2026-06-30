e8faeb9b58013f227c133d3ab54328fc96f2be1c
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-06-30 00:42:16 -0400

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the first symbol");
+string first = Console.ReadLine();
+Console.WriteLine(first + " " + first);
+Console.WriteLine(" " + first);
+Console.WriteLine(first + " " + first);
+
+Console.WriteLine("Enter the second symbol");
+first = Console.ReadLine();
+Console.WriteLine(first + "." + first + ".." + first);
