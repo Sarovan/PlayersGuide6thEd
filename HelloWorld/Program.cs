40bf0545439bd7cdb7a59afffa6e52e155bceab7
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-07-22 00:03:45 -0400

    starting Classes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,48 +1,8 @@
-﻿int roundNumber = 1;
-int damagedStructures = 0;
-int damageToUmbra = 0;
-int distanceToUmbra = Random.Shared.Next(25, 75);
-int range = 0;
+﻿Countdown(10);
 
-while (damagedStructures < 20 && damageToUmbra < 20 && distanceToUmbra > 0)
+void Countdown(int num)
 {
-    string display = $"""
----------------- STATUS -----------------
-Round: {roundNumber}
-Structures Destroyed: {damagedStructures}
-Umbra Damage: {damageToUmbra}
-A hit will deal {DamageDealtToUmbra(roundNumber)} damage right now.
------------------------------------------
-""";
-
-    Console.WriteLine(display);
-
-    do
-    {
-        Console.Write("Enter a range to hit (between 0 and 100): ");
-        range = int.Parse(Console.ReadLine());
-    }
-    while (range < 0 || range > 100);
-
-    if (range < distanceToUmbra) Console.WriteLine("That fell short!");
-    else if (range > distanceToUmbra) Console.WriteLine("That went too far!");
-    else
-    {
-        Console.WriteLine("That was a direct hit!");
-        damageToUmbra += DamageDealtToUmbra(roundNumber);
-    }
-
-    roundNumber++;
-    damagedStructures++;
-    distanceToUmbra -= Random.Shared.Next(1, 3);
-}
-
-if (range > 0) Console.WriteLine($"{(damageToUmbra >= 20 ? "Player" : "Umbra")} won!");
-else Console.WriteLine("The Umbra smashed your face!");
-
-int DamageDealtToUmbra(int roundNumber)
-{
-    if (roundNumber % 3 == 0 && roundNumber % 5 == 0) return 5;
-    else if (roundNumber % 3 == 0 || roundNumber % 5 == 0) return 3;
-    else return 1;
+    if (num == 0) return;
+    Console.WriteLine(num);
+    Countdown(num - 1);
 }
---
+++ SixStaffs/Program.cs
@@ -0,0 +1,34 @@
+﻿string tekhelet = "\e[38;2;89;12;131mT\e[39m";
+string mauveine = "\e[38;2;143;48;161mM\e[39m";
+string amaranth = "\e[38;2;240;24;79mA\e[39m";
+string jasmine = "\e[38;2;246;215;141mJ\e[39m";
+string keppel = "\e[38;2;70;179;165mK\e[39m";
+string bice = "\e[38;2;46;109;146mB\e[39m";
+
+// Console.WriteLine(tekhelet);
+// Console.WriteLine(mauveine);
+// Console.WriteLine(amaranth);
+// Console.WriteLine(jasmine);
+// Console.WriteLine(keppel);
+// Console.WriteLine(bice);
+
+Henge[] hengeArray = [Henge.Tekhelet, Henge.Mauveine, Henge.Amaranth, Henge.Jasmine, Henge.Keppel, Henge.Bice];
+
+foreach (Henge henge in hengeArray)
+{
+    (int x, int y, string symbol) = GetInformation(henge);
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(symbol);
+}
+
+(int, int, string) GetInformation(Henge henge) => henge switch
+{
+    Henge.Tekhelet => (0b1000, 0b0000, tekhelet),
+    Henge.Mauveine => (0b1011, 0b0011, mauveine),
+    Henge.Amaranth => (0b0000, 0b0111, amaranth),
+    Henge.Jasmine => (0b0011, 0b0011, jasmine),
+    Henge.Keppel => (0b0111, 0b0111, keppel),
+    Henge.Bice => (0b1110, 0b0111, bice),
+};
+
+enum Henge { Tekhelet, Mauveine, Amaranth, Jasmine, Keppel, Bice }
