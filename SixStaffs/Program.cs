05aec26c750cfab62dde2a851f46826ffd6d0dc1
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-07-27 21:29:43 -0400

    finished challenge

--- SixStaffs/Program.cs
+++ SixStaffs/Program.cs
@@ -1,9 +1,9 @@
-﻿string tekhelet = "\e[38;2;89;12;131mT\e[39m";
-string mauveine = "\e[38;2;143;48;161mM\e[39m";
-string amaranth = "\e[38;2;240;24;79mA\e[39m";
-string jasmine = "\e[38;2;246;215;141mJ\e[39m";
-string keppel = "\e[38;2;70;179;165mK\e[39m";
-string bice = "\e[38;2;46;109;146mB\e[39m";
+﻿// string tekhelet = "\e[38;2;89;12;131mT\e[39m";
+// string mauveine = "\e[38;2;143;48;161mM\e[39m";
+// string amaranth = "\e[38;2;240;24;79mA\e[39m";
+// string jasmine = "\e[38;2;246;215;141mJ\e[39m";
+// string keppel = "\e[38;2;70;179;165mK\e[39m";
+// string bice = "\e[38;2;46;109;146mB\e[39m";
 
 // Console.WriteLine(tekhelet);
 // Console.WriteLine(mauveine);
@@ -12,31 +12,49 @@ string bice = "\e[38;2;46;109;146mB\e[39m";
 // Console.WriteLine(keppel);
 // Console.WriteLine(bice);
 
-Henge[] hengeArray = [Henge.Tekhelet, Henge.Mauveine, Henge.Amaranth, Henge.Jasmine, Henge.Keppel, Henge.Bice];
+// Henge[] hengeArray = [Henge.Tekhelet, Henge.Mauveine, Henge.Amaranth, Henge.Jasmine, Henge.Keppel, Henge.Bice];
 
-foreach (Henge henge in hengeArray)
-{
-    (int x, int y, string symbol) = GetInformation(henge);
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine(symbol);
-}
+// foreach (Henge henge in hengeArray)
+// {
+//     (int x, int y, string symbol) = GetInformation(henge);
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine(symbol);
+// }
 
-(int, int, string) GetInformation(Henge henge) => henge switch
-{
-    Henge.Tekhelet => (0b1000, 0b0000, tekhelet),
-    Henge.Mauveine => (0b1011, 0b0011, mauveine),
-    Henge.Amaranth => (0b0000, 0b0111, amaranth),
-    Henge.Jasmine => (0b0011, 0b0011, jasmine),
-    Henge.Keppel => (0b0111, 0b0111, keppel),
-    Henge.Bice => (0b1110, 0b0111, bice),
-};
+// (int, int, string) GetInformation(Henge henge) => henge switch
+// {
+//     Henge.Tekhelet => (0b1000, 0b0000, tekhelet),
+//     Henge.Mauveine => (0b1011, 0b0011, mauveine),
+//     Henge.Amaranth => (0b0000, 0b0111, amaranth),
+//     Henge.Jasmine => (0b0011, 0b0011, jasmine),
+//     Henge.Keppel => (0b0111, 0b0111, keppel),
+//     Henge.Bice => (0b1110, 0b0111, bice),
+// };
 
-enum Henge { Tekhelet, Mauveine, Amaranth, Jasmine, Keppel, Bice }
+// enum Henge { Tekhelet, Mauveine, Amaranth, Jasmine, Keppel, Bice }
 
+Location amaranthLocation = new Location(3, 2);
+Color amaranthColor = new Color(240, 24, 79);
+Char amaranthChar = 'A';
+Henge amaranth = new Henge(amaranthLocation, amaranthColor, amaranthChar);
+
+Location jasmineLocation = new Location(1, 5);
+Color jasmineColor = new Color(246, 215, 141);
+Char jasmineChar = 'J';
+Henge jasmine = new Henge(jasmineLocation, jasmineColor, jasmineChar);
+
+amaranth.Display();
+jasmine.Display();
 public class Location
 {
     public int X { get; set; }
     public int Y { get; set; }
+
+    public Location(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
 }
 
 public class Color
