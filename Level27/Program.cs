737444dbf0029d2473a519222f59f61aa41dd101
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-08-08 13:21:38 -0400

    finished 28

---
+++ Level27/Program.cs
@@ -0,0 +1,86 @@
+﻿Robot robot = new Robot();
+
+for (int i = 0; i < 3; i++)
+{
+    string input = Console.ReadLine();
+    robot.Commands[i] = input switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand(),
+    };
+}
+
+robot.Run();
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y += 1;
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y -= 1;
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X -= 1;
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X += 1;
+    }
+}
---
+++ Level28/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Coordinate first = new Coordinate(2, 3);
+Coordinate second = new Coordinate(2, 4);
+Coordinate third = new Coordinate(5, 5);
+
+Console.WriteLine(IsAdjacent(first, second));
+Console.WriteLine(IsAdjacent(first, third));
+Console.WriteLine(IsAdjacent(third, second));
+
+
+bool IsAdjacent(Coordinate a, Coordinate b)
+{
+    if (Math.Abs(a.Row - b.Row) > 1) return false;
+    if (Math.Abs(a.Column - b.Column) > 1) return false;
+    return true;
+}
+
+public struct Coordinate
+{
+    public int Row { get; }
+    public int Column { get; }
+
+    public Coordinate(int x, int y)
+    {
+        Row = x;
+        Column = y;
+    }
+}
