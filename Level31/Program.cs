f30091c4a84fee9ded37c6366aed0b10332407d5
Sarovan <44712196+Sarovan@users.noreply.github.com>
2026-08-16 00:17:29 -0400

    finished main code of 31

--- Level31/Program.cs
+++ Level31/Program.cs
@@ -1,4 +1,7 @@
-﻿Point[,] grid = new Point[4, 4];
+﻿using System.Data;
+using System.Dynamic;
+
+Point[,] grid = new Point[4, 4];
 
 for (int i = 0; i < 4; i++)
 {
@@ -17,15 +20,62 @@ while (true)
     Console.WriteLine($"You are in the room at (Row={player.Point.Row}, Column={player.Point.Column}).");
     if (player.Point == Entrance)
     {
+        if (fountain.IsOn)
+        {
+            Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!");
+            Console.WriteLine("You win!");
+            break;
+        }
         Console.WriteLine("You see light coming from the cavern entrance.");
+    }
+    else if (player.Point == fountain.Point)
+    {
+        if (fountain.IsOn)
+        {
+            Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
+        }
+        else
+            Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
     }
     Console.Write("What do you want to do? ");
     string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "east":
+        case "west":
+        case "north":
+        case "south":
+            player.Move(choice);
+            break;
+        case "enable":
+            fountain.IsOn = true;
+            break;
+    }
 }
 
 public class Player(int row, int column)
 {
     public Point Point { get; set; } = new Point(row, column);
+
+    public void Move(string direction)
+    {
+        if (direction == "east" && Point.Column < 3)
+        {
+            Point.Column++;
+        }
+        else if (direction == "west" && Point.Column > 0)
+        {
+            Point.Column--;
+        }
+        else if (direction == "north" && Point.Row > 0)
+        {
+            Point.Row--;
+        }
+        else if (direction == "south" && Point.Row < 3)
+        {
+            Point.Row++;
+        }
+    }
 }
 
 public class Fountain(int row, int column)
@@ -34,4 +84,8 @@ public class Fountain(int row, int column)
     public bool IsOn { get; set; } = false;
 }
 
-public record Point(int Row, int Column);
+public record Point(int Row, int Column)
+{
+    public int Row { get; set; } = Row;
+    public int Column { get; set; } = Column;
+}
